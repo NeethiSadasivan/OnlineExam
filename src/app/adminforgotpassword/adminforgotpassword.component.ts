41c47610884863dad638a4f4527cb07edbab3056
NeethiSadasivan <neethinandu1@gmail.com>
2021-09-25 18:48:07 +0530

    menu, button, footer, background, font changes

--- src/app/adminforgotpassword/adminforgotpassword.component.ts
+++ src/app/adminforgotpassword/adminforgotpassword.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Admin } from '../admin';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-adminforgotpassword',
   templateUrl: './adminforgotpassword.component.html',
   styleUrls: ['./adminforgotpassword.component.css']
 })
 export class AdminforgotpasswordComponent implements OnInit {
+  AdminfpForm:FormGroup=new FormGroup({
+    emailid:new FormControl("",[Validators.required,Validators.pattern("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")]),
+  });
   admin!:Admin
   constructor() { }
 
   ngOnInit(): void {
     this.admin = new Admin()
+  }
+  get emailid()
+  {
+    return this.AdminfpForm.get('emailid');
+  }
+  get password()
+  {
+    return this.AdminfpForm.get('password');
   }
 
   Submitdata()
--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { Admin } from '../admin';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-adminlogin',
   templateUrl: './adminlogin.component.html',
   styleUrls: ['./adminlogin.component.css']
 })
 export class AdminloginComponent implements OnInit {
+  AdminLoginForm:FormGroup=new FormGroup({
+    emailid:new FormControl("",[Validators.required,Validators.pattern("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$")]),
+    password:new FormControl("",[Validators.required])
+  });
   admin!:Admin
   constructor() { }
 
   ngOnInit(): void {
     this.admin = new Admin()
+  }
+  get emailid()
+  {
+    return this.AdminLoginForm.get('emailid');
+  }
+  get password()
+  {
+    return this.AdminLoginForm.get('password');
   }
   Submitdata()
   {
