7654e3d92777fb179bfbc5a7a00a939bfacfe702
kgoel99 <kunalgoel25061999@gmail.com>
2021-10-03 22:30:47 +0530

    showing student details

--- src/app/admin-single-student/admin-single-student.component.ts
+++ src/app/admin-single-student/admin-single-student.component.ts
@@ -33,10 +33,12 @@ export class AdminSingleStudentComponent implements OnInit {
   ngOnInit(): void {  }
   
   isShown : boolean = false;
+
   SearchStudent()
   {
-    this.isShown = true;
+    
     this.aiService.getStudentResultsByState(this.resForm.value.subjectid,this.resForm.value.state).subscribe((data)=>{
+      this.isShown = true;
       this.result=data;
       console.log((data));
     })
